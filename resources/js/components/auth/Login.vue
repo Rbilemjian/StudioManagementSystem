f8c4a3b746d67bb05b7929d1c0ac14aaaeab6dc3
Unknown <rbilemjian@gmail.com>
2018-12-12 15:57:39 -0800

    Added Laravel Dusk for Frontend Testing

--- resources/js/components/auth/Login.vue
+++ resources/js/components/auth/Login.vue
@@ -1,31 +1,33 @@
 <template>
-    <div class="login row justify-content-center">
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-header">Login</div>
-                <div class="card-body">
-                    <form @submit.prevent="authenticate">
-                        <div class="form-group-row" style="margin-top:10px;">
-                            <label for="email">Email</label>
-                            <input type="email" v-model="form.email" class="form-control" placeholder="Email Address">
-                        </div>
-                        <div class="form-group-row" style="margin-top:10px;">
-                            <label for="password">Password</label>
-                            <input type="password" v-model="form.password" class="form-control" placeholder="Password">
-                        </div>
-                        <div class="form-group-row text-center" style="margin-top:20px;">
-                            <input type="submit" class="btn btn-primary btn-md"><span style="padding-left: 7px" class="glyphicon glyphicon-login" aria-hidden="true"></span></button>
-                        </div>
-                        <div class="form-group-row" v-if="authError" style="margin-top:20px; margin-bottom:5px;">
-                            <p class="error">
-                                {{ authError }}
-                            </p>
-                        </div>
-                    </form>
+    <body>
+        <div class="login row justify-content-center">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header">Login</div>
+                    <div class="card-body">
+                        <form @submit.prevent="authenticate">
+                            <div class="form-group-row" style="margin-top:10px;">
+                                <label for="email">Email</label>
+                                <input type="email" id="email" v-model="form.email" class="form-control" placeholder="Email Address">
+                            </div>
+                            <div class="form-group-row" style="margin-top:10px;">
+                                <label for="password">Password</label>
+                                <input type="password" id="password" v-model="form.password" class="form-control" placeholder="Password">
+                            </div>
+                            <div class="form-group-row text-center" style="margin-top:20px;">
+                                <input type="submit" id="login-submit" class="btn btn-primary btn-md"><span style="padding-left: 7px" class="glyphicon glyphicon-login" aria-hidden="true"></span></button>
+                            </div>
+                            <div class="form-group-row" v-if="authError" style="margin-top:20px; margin-bottom:5px;">
+                                <p class="error">
+                                    {{ authError }}
+                                </p>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </body>
 </template>
 
 <script>
